5164ea60d7da2c25c9efaed6bbd4f50d6ccb7f3d
Divya Gupta <divya.gupta@intimetec.com>
2016-04-28 18:25:03 +0530

    create/remove todo notes task1

--- js/Todo.js
+++ js/Todo.js
@@ -1,21 +1,22 @@
 var i = 0,
-		todos = [];
+		todos = [],
+		trash = [];
 
 function checkForExistingNotes(){
 	var todo  = JSON.parse(localStorage.getItem("Todo"));
 	if(todo){
 		i = todo.length;
-		return todo;
+		todos = todo;
 	};
 }
 // create Notes
 function createNewNote(){
-	todos = checkForExistingNotes();
+	checkForExistingNotes();
 	id = i;
 
 	var title = document.forms.Todo.title.value,
 		data =  document.forms.Todo.notes.value,
-		timestamp = new Date();	
+		timestamp = new Date().toDateString();	
 
 	
 	todos.push({
@@ -26,19 +27,40 @@ function createNewNote(){
 		 "type": "Notes"
 	});
 	localStorage.setItem("Todo",JSON.stringify(todos));
-	document.getElementById('notes').innerHTML +="<div><h3>"+todos[todos.length - 1].title + 
-		"</h3><p>" + todos[todos.length - 1].data  + "<p>Created At " + todos[todos.length - 1].timestamp + "</p></div><br/>" 
+	document.getElementById('title').value = "";
+	document.getElementById('desc').value = "";
+	document.getElementById('notes').innerHTML +="<div><h3>"+todos[todos.length - 1].title + "</h3><p>" + todos[todos.length - 1].data  + "<p>Created At " + todos[todos.length - 1].timestamp + "</p><input type='button' value='Delete' onclick='removeNotes(" + todos[todos.length - 1].id +")'/></div>";
 }
 
 // Get Notes
-function getNotes(todos){
-		todos = checkForExistingNotes();
+function getNotes(){
+	document.getElementById('notes').innerHTML = "";
+		var todo  = JSON.parse(localStorage.getItem("Todo"));
+		if(todo !== null ){
+		i = todo.length;
+		todos = todo;
 		for(var key = 0; key < todos.length ; key++){
-		document.getElementById('notes').innerHTML +="<div><h3>"+todos[key].title + 
-		"</h3><p>" + todos[key].data + "</p> " + "<p>Created At " + todos[key].timestamp +"</p></div><br/>" 
+		document.getElementById('notes').innerHTML +="<div><h3>"+todos[key].title + "</h3><p>" + todos[key].data  + "<p>Created At " + todos[key].timestamp + "</p><input type='button' value='Delete' onclick='removeNotes(" + key +")'/></div>";
+		}
+		
 	}
 	
 }
+
+// Remove Notes
+function removeNotes(id){
+	var todo  = JSON.parse(localStorage.getItem("Todo"));
+	var trashedItem = todo.splice(id,1)
+	for(var i=0; i< trashedItem.length; i++)
+		trash.push(trashedItem[i]);
+	localStorage.setItem("Todo",JSON.stringify(todo));
+	localStorage.setItem("Trash", JSON.stringify(trash));
+	getNotes()
+	if(!todo.length){
+		localStorage.removeItem("Todo");
+	}
+}
+
 $( document ).ready(function() {
 	getNotes();
 });
